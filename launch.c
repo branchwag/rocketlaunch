46e99c6383b0db0686933a6f28d9ed64c61934ba
branchwag <branchleannelmb@gmail.com>
2024-10-22 18:35:03 -0400

    Gets triangleand rectangle drawn

--- launch.c
+++ launch.c
@@ -1,7 +1,4 @@
 #include "raylib.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
@@ -9,10 +6,20 @@
 int main(void) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Rocket Launch");
 	SetTargetFPS(60);
+
+	Vector2 rocketPosition = { SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT - 130 };
+	float rocketSpeed = 5.0f;
 	
 	while (!WindowShouldClose()) {
+		if (rocketPosition.y > 0) {
+			rocketPosition.y -= rocketSpeed;
+		}
+		
 		BeginDrawing();
 		ClearBackground(BLACK);
+
+		DrawRectangle(rocketPosition.x, rocketPosition.y, 50, 100, DARKGRAY);
+		DrawTriangle((Vector2){ 400, 200 }, (Vector2){ 300, 600}, (Vector2){ 500, 600 }, RED);
 		EndDrawing();
 	}
 
