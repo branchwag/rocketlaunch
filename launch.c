e24376773017fab630931ee309325281612ec5c8
branchwag <branchleannelmb@gmail.com>
2024-10-22 22:55:39 -0400

    makes triangle top of rocket

--- launch.c
+++ launch.c
@@ -7,7 +7,7 @@ int main(void) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Rocket Launch");
 	SetTargetFPS(60);
 
-	Vector2 rocketPosition = { SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT - 130 };
+	Vector2 rocketPosition = { SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT - 100 };
 	float rocketSpeed = 5.0f;
 	
 	while (!WindowShouldClose()) {
@@ -19,7 +19,14 @@ int main(void) {
 		ClearBackground(BLACK);
 
 		DrawRectangle(rocketPosition.x, rocketPosition.y, 50, 100, DARKGRAY);
-		DrawTriangle((Vector2){ 400, 200 }, (Vector2){ 300, 600}, (Vector2){ 500, 600 }, RED);
+
+		Vector2 triangleBase = { rocketPosition.x + 25, rocketPosition.y };
+		Vector2 triangleLeft = { rocketPosition.x, rocketPosition.y };
+		Vector2 triangleRight = { rocketPosition.x + 50, rocketPosition.y };
+		Vector2 triangleTop = { triangleBase.x, rocketPosition.y - 50 };
+
+		DrawTriangle(triangleTop, triangleLeft, triangleRight, RED);
+
 		EndDrawing();
 	}
 
